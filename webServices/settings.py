267ed89f9db0f4503bf69d30b9762fe8bbe13c80
Alfonso Guerrero <sc22ag2@leeds.ac.uk>
2025-03-02 15:00:12 +0000

    static_fixes

--- webServices/settings.py
+++ webServices/settings.py
@@ -125,12 +125,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = []
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
